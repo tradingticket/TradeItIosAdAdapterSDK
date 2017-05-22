941ea7d45fc51ea76e40e0559cb48973c8dcc36b
James Robert Somers <james@trade.it>
2017-05-22 10:30:05 -0400

    New project

---
+++ TradeItIosAdAdapterSDK/TradeItIosAdAdapterSDK.h
@@ -0,0 +1,19 @@
+//
+//  TradeItIosAdAdapterSDK.h
+//  TradeItIosAdAdapterSDK
+//
+//  Created by James Robert Somers on 22/5/17.
+//  Copyright © 2017 TradeIt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TradeItIosAdAdapterSDK.
+FOUNDATION_EXPORT double TradeItIosAdAdapterSDKVersionNumber;
+
+//! Project version string for TradeItIosAdAdapterSDK.
+FOUNDATION_EXPORT const unsigned char TradeItIosAdAdapterSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TradeItIosAdAdapterSDK/PublicHeader.h>
+
+
